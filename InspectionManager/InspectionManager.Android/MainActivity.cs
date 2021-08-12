5ae146ff12d594ac55c1f8db2ff6cf8012c9c3ae
gonzale1811 <david_1811_@hotmail.com>
2021-08-12 18:39:43 +0200

    Inicializado Firebase en ambos proyectos

--- InspectionManager/InspectionManager.Android/MainActivity.cs
+++ InspectionManager/InspectionManager.Android/MainActivity.cs
@@ -6,19 +6,22 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Firebase;
 
 namespace InspectionManager.Droid
 {
     [Activity(Label = "InspectionManager", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static FirebaseApp app;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
+            InitFirebaseAuth();
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
@@ -28,6 +31,17 @@ namespace InspectionManager.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder().
+                SetApplicationId("inspection-manager-609e2").
+                SetApiKey("AIzaSyCU0_ZBKeSNvdZMvpWnsBQW8zXQqZEEDD4")
+                .SetDatabaseUrl("https://inspection-manager-609e2-default-rtdb.europe-west1.firebasedatabase.app/").Build();
+
+            if (app == null)
+                app = FirebaseApp.InitializeApp(this, options, "FirebaseSample");
         }
     }
 }
--- InspectionManager/InspectionManager.iOS/AppDelegate.cs
+++ InspectionManager/InspectionManager.iOS/AppDelegate.cs
@@ -24,7 +24,7 @@ namespace InspectionManager.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
-
+            Firebase.Core.App.Configure();
             return base.FinishedLaunching(app, options);
         }
     }
